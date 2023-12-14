a6aac6dc9193f13e24ffba454e40dec3052f63aa
Khaya Khumalo <khaya.khumalo93@gmail.com>
2023-12-14 23:11:41 +0000

    first commit

--- index.php
+++ index.php
@@ -17,7 +17,45 @@
             flex-direction: column;
             align-items: center;
             justify-content: center;
+            background-image: url('phone.png');
+            background-repeat: no-repeat;
+            background-size: contain;
+            /* or 'cover' if you want it to cover the entire viewport */
+            background-position: center center;
+            background-attachment: fixed;
+            /* optional, to make the background fixed */
+            margin: 0;
+            /* remove default body margin */
             height: 100vh;
+            /* make sure the body takes up the full viewport height */
+            display: flex;
+            align-items: center;
+        }
+
+        body::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: radial-gradient(circle, rgba(255, 255, 255, 0) 20%, rgba(255, 255, 255, 0.6) 60%);
+            pointer-events: none;
+            /* allows clicking through the pseudo-element */
+        }
+
+
+        .well {
+            background-color: rgba(200, 200, 200, 0.7);
+            /* Adjust the last value for transparency (0 to 1) */
+            color: white;
+            padding: 20px;
+            padding-bottom: 40px;
+            width: 50%;
+            margin: 100px auto;
+            /* Adjust the margin as needed */
+            border-radius: 10px;
+            text-align: center;
         }
 
         h1 {
@@ -61,14 +99,17 @@
 </head>
 
 <body>
-    <h1>Gontrel</h1>
+    <div class="well">
+        <h1>Gontrel</h1>
 
-    <p>Level up your life with Gontrel's game-changing experience! Explore epic events, discover thrilling activities, and savor unforgettable meals at top-notch restaurants. Ready to upgrade? Register your interest now!</p>
+        <p style="margin: auto; padding-bottom: 20px">Level up your life with Gontrel's game-changing experience! Explore epic events, discover thrilling activities,
+            and savor unforgettable meals at top-notch restaurants. Ready to upgrade? Register your interest now!</p>
 
-    <form>
-        <input type="email" placeholder="Enter your email" required>
-        <button type="submit">Submit</button>
-    </form>
+        <form style="margin: auto;">
+            <input type="email" placeholder="Enter your email" required>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
 </body>
 
 </html>
